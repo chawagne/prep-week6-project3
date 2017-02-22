aeb132dd166c079e7b5b8fb9c46f93418041a2b3
Chad Wagner <chawagne@gmail.com>
2017-02-21 18:17:05 -0700

    all but 7

--- main.js
+++ main.js
@@ -10,35 +10,58 @@
 */
 
 
-$(document).ready(function() {  
-   
+$(document).ready(function() {
 
-    
+
+
+// Question #3
+// $("button.changeColor").on("click", function(){
+//     $("ul li:nth-child(3)").addClass("randomColor");
+//
+// } )
+$(".changeColor").click(function(){
+  $("ul li:nth-child(3)").css("color","#8A2BE2");
+});
+
 // Question #1
+$("ul li:nth-child(4)").on("click", function () {
+  $(this).append("<li>Aaaayyyyyy!</li>");
+})
 
 
 // Question #2
 
-
-// Question #3
-
-
+$("#removeLi").on("click", function () {
+  $("ul li:nth-child(2)").remove();
+})
 // Question #4
-    
-    
-// Question #5  
-  
+$("ul li:last").on("click", function() {
+    $("ul li").hide(0);
+    $("ul li:last").show().css("font-size", "40px");
+})
 
-  // Question #6	
-	
+// Question #5
+$(".cute").on("click", function () {
+  $(".clones").append($(".cute").clone(true));
+})
 
-	
-// Question #7
-  
-  
-  
+  // Question #6
+$("body").on("dblclick", function () {
+  $("#makeSquare").css("border-radius","0px").css("width","120px");
+})
+
+// Question #7 WIP
+// $(".swatches").each(function(){
+//             alert($(this).())})
+
+
   // Question #8
-
+$("#hover").hover(function (){
+  $("#hover").css("background-color","red");
+    $("#hover").on("click", function () {
+      $("#hover").toggleClass("green");
+  })
+});
 
 
 
